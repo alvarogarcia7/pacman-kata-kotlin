6e54090002adc599368ed196c1d2dd4c3e37e8f6
Alvaro Garcia <derecerca@gmail.com>
2017-08-31 00:37:37 +0200

    Red: wrap laterally on the grid (left to right)

--- src/test/kotlin/com/kata/pacman/PacmanOnAGridTest.kt
+++ src/test/kotlin/com/kata/pacman/PacmanOnAGridTest.kt
@@ -14,6 +14,11 @@ class PacmanOnAGridTest {
     }
 
     @Test
+    fun `wrap the position - from leftmost to rightmost`() {
+        movePacman(at(0, 0), facing(Direction.LEFT), endsUp(at(1,0)))
+    }
+
+    @Test
     fun `wrap the position - from rightmost to leftmost, when past the border`() {
         movePacman(at(2, 0), facing(Direction.RIGHT), endsUp(at(2+1-2,0)))
     }
