c0b41843d5fea9fd874563e86322d3229a4d1934
Alvaro Garcia <derecerca@gmail.com>
2017-09-03 15:24:28 +0200

    Refactor: move functions to outside scope

--- src/main/kotlin/com/kata/pacman/Pacman.kt
+++ src/main/kotlin/com/kata/pacman/Pacman.kt
@@ -37,27 +37,27 @@ data class Position(var x: Int, var y: Int) {
 class Grid(val dimension: Dimension) {
 
     fun  simplify(position: Position): Position {
-        var result = position
-        fun wrapX(position: Position, threshold: Int): Position {
-            if(position.beforeTheX()){
-                return increaseXAxis(position)
-            } else if(position.atOrPastTheX(threshold)){
-                return decreaseXAxis(position)
-            }
-            return position
-        }
-
-        fun wrapY(position: Position, threshold: Int) : Position {
-            if (position.beforeTheY()) {
-                return increaseYAxis(position)
-            } else if (position.atOrPastTheY(threshold)) {
-                return decreaseYAxis(position)
-            }
-            return position
-        }
-        return wrapY(wrapX(result, dimension.x), dimension.y)
+        return wrapY(wrapX(position, dimension.x), dimension.y)
     }
 
+
+    private fun wrapX(position: Position, threshold: Int): Position {
+        if (position.beforeTheX()) {
+            return increaseXAxis(position)
+        } else if (position.atOrPastTheX(threshold)) {
+            return decreaseXAxis(position)
+        }
+        return position
+    }
+
+    private fun wrapY(position: Position, threshold: Int) : Position {
+        if (position.beforeTheY()) {
+            return increaseYAxis(position)
+        } else if (position.atOrPastTheY(threshold)) {
+            return decreaseYAxis(position)
+        }
+        return position
+    }
 
     private fun decreaseXAxis(position: Position): Position = position.copy(x = position.x - dimension.x)
     private fun increaseXAxis(position: Position): Position = position.copy(x = position.x + dimension.x)
